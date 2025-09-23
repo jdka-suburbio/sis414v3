b823ba4aef9a5f53fd03fea3f8077d39864a1c68
garyfloo <garyvilca123@gmail.com>
2025-09-23 12:53:38 -0600

    tarea 3 subida :)

---
+++ VillcaGary/tarea3/G2/G2/src/test/java/Siistemas/_4/G2/Sis414G2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Siistemas._4.G2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sis414G2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
