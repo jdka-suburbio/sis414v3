27c4670b12b27c72513adb48247b5529ed4f85ab
garyfloo <garyvilca123@gmail.com>
2025-10-09 00:29:02 -0600

    añadiendo tarea4

---
+++ VillcaGary/Tarea4/src/test/java/Practica_4/Terea_4/Terea4ApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica_4.Terea_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Terea4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
